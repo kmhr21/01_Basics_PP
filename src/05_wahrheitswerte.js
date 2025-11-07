4804af59971d24c59728a01b433633ba5a4edff3
Peter Schmidt <schmidt.multimedia@gmail.com>
2025-11-07 10:44:14 +0000

    05 / Berechnung Alter Iteration II

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -6,10 +6,20 @@ let ageJohn, ageMark;
 const birthYearJohn = 2000;
 const birthYearMark = 1990;
 
-// Berechnung Alter
-let year = 2025;
+// Berechnung Alter Iteration I
+// let year = 2025;
+// ageJohn = year - birthYearJohn;
+// ageMark = year - birthYearMark;
+
+// Berechnung Alter Iteration II
+let date = new Date();
+let year = date.getFullYear();
+console.log("Datum: " + date);
+console.log("Jahr: " + year);
 ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
+
+
 
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
